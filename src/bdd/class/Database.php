a80003b58ecd054a86e627f264f156a6c4596531
Mathias Genibrel <mathias.geni@gmail.com>
2024-01-29 15:45:37 +0100

    fix: docker-compose.yaml

--- src/bdd/class/Database.php
+++ src/bdd/class/Database.php
@@ -1,7 +1,6 @@
 <?php
 class Database {
-    private $host = "127.0.0.1";
-    private int $port = 3306;
+    private $host = "database";
     private $dbname = "SocialSport";
     private $username = "root";
     private $password = "root";
@@ -9,7 +8,7 @@ class Database {
 
     public function __construct() {
         try {
-            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname};port={$this->port}", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Échec de la connexion : " . $e->getMessage());
